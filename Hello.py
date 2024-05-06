bd2f2d06d848c62a94ca19519549c1835a8aed6e
SuharshanaPeiris <heshan.20221171@iit.ac.lk>
2024-05-06 13:02:37 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -49,3 +49,11 @@ def run():
 
 if __name__ == "__main__":
     run()
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+
+st.set_page_config(page_title="Global Superstore", page_icon=":bar_chart:",layout="wide")
+
+st.title(" :bar_chart: Sample Global Superstore EDA")
