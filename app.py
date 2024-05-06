99a78575d612d6c2a69c5ed61ff292797116f5d4
SuharshanaPeiris <heshan.20221171@iit.ac.lk>
2024-05-06 13:01:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import os
+
+st.set_page_config(page_title="Global Superstore", page_icon=":bar_chart:",layout="wide")
+
+st.title(" :bar_chart: Sample Global Superstore EDA")
