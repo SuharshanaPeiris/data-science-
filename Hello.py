45c8dfa2113985e5e256da4abafbc32760faf88f
SuharshanaPeiris <heshan.20221171@iit.ac.lk>
2024-05-06 16:16:35 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -46,6 +46,10 @@ import matplotlib.pyplot as plt
 
 
 
+   uploaded_file = st.file_uploader("Processed_GlobalSuperstore.csv", type=["csv"])
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
     # Check the first few rows of the dataframe
     st.write(df.head())
 
